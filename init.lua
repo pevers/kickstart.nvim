3c0b1df7800f74888fd71fa6f3b8ae1a79041edc
Peter Evers <peter.evers@mistral.ai>
2026-03-30 22:57:25 +0200

    chore: add Python LSP with ruff

--- init.lua
+++ init.lua
@@ -602,9 +602,10 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        ruff = {},
         -- rust_analyzer = {},
-        --
+        -- --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
@@ -653,6 +654,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         -- You can add other tools here that you want Mason to install
+        'debugpy',
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
